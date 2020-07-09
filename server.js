f7a1e3be56c1ad2ad2890cd1a92131eb96f72ef3
JJ Cardenas <jjcardenas16@hotmail.com>
2020-07-09 16:34:36 -0700

    updated line 15 of server.js to app.use(express.static(client/public))

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors())
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
